5f523f925f1d89359064d2d10385e802ffe55961
chang0731 <71751403+chang0731@users.noreply.github.com>
2020-10-27 00:50:24 +0800

    一半

--- main.ts
+++ main.ts
@@ -5,9 +5,9 @@ basic.forever(function () {
             basic.pause(200)
         }
     }
-    for (let i = 0; i <= 4; i++) {
-        for (let j = 0; j <= i; j++) {
-            led.plot(4 - i + j, j + 1)
+    for (let k = 0; k <= 4; k++) {
+        for (let l = 0; l <= k; l++) {
+            led.plot(4 - k + l, l + 1)
             basic.pause(200)
         }
     }
