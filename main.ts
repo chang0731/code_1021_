263518ff5f84af8b65092121ec29cca4e0b7beef
chang0731 <71751403+chang0731@users.noreply.github.com>
2020-10-27 18:55:33 +0800

    上半部

--- main.ts
+++ main.ts
@@ -1,14 +1,20 @@
-basic.forever(function () {
-    for (let i = 0; i <= 4; i++) {
-        for (let j = 0; j <= i; j++) {
-            led.plot(4 - i + j, j)
-            basic.pause(200)
+let jj = 0
+let yy = 0
+let xx = 0
+let ii = 0
+let index = 0
+ii += 1
+for (let list = 0; list <= 4; list++) {
+    xx = 4 - list
+    yy = 4 - list
+    ii = 0
+    while (ii <= 4) {
+        jj = 0
+        while (jj <= yy) {
+            led.plot(xx - jj, jj)
+            jj += 1
         }
+        ii += 1
     }
-    for (let k = 0; k <= 4; k++) {
-        for (let l = 0; l <= k; l++) {
-            led.plot(4 - k + l, l + 1)
-            basic.pause(200)
-        }
-    }
-})
+    basic.pause(200)
+}
