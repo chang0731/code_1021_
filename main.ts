7634b129652dd826d288cad36c66b99215b24d55
chang0731 <71751403+chang0731@users.noreply.github.com>
2020-10-28 09:17:59 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,15 @@
-let jj = 0
-let yy = 0
-let xx = 0
-let ii = 0
-let index = 0
-ii += 1
-for (let list = 0; list <= 4; list++) {
-    xx = 4 - list
-    yy = 4 - list
-    ii = 0
-    while (ii <= 4) {
-        jj = 0
-        while (jj <= yy) {
-            led.plot(xx - jj, jj)
-            jj += 1
+basic.forever(function () {
+    for (let i = 0; i <= 4; i++) {
+        for (let j = 0; j <= i; j++) {
+            led.plot(4 - i + j, 4 - j)
         }
-        ii += 1
+        basic.pause(200)
     }
-    basic.pause(200)
-}
+    for (let i = 0; i <= 4; i++) {
+        for (let j = 0; j <= 3 - i; j++) {
+            led.plot(j, 3 - i - j)
+        }
+        basic.pause(200)
+    }
+    basic.clearScreen()
+})
